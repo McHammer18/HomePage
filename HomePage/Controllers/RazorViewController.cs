27eee028fc43c190045cf0ebecb025b0adc4dcad
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-10-03 17:40:06 -0500

    Adding drop down menu to the nav bar student model and controller createted for assignment 6.1

---
+++ HomePage/Controllers/RazorViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomePage.Controllers
+{
+    public class RazorViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HomePage/Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomePage.Models
+{
+    public class StudentModel
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Grade { get; set; }
+    }
+}
