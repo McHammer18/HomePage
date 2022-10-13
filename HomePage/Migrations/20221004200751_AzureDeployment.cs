6fb37718ac6d86e58731cae9b37a87308c8a5bc6
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-10-12 19:49:30 -0500

    New assignment link

---
+++ HomePage/Migrations/20221004200751_AzureDeployment.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomePage.Migrations
+{
+    public partial class AzureDeployment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
